482cd5e5f311aec4fc2b6214efab02cc0d08a041
Jonakls <asmot54@gmail.com>
2022-01-23 01:55:30 -0500

    feat: added abstractions of api project

---
+++ api/src/main/java/me/jonakls/api/GameStatus.java
@@ -0,0 +1,9 @@
+package me.jonakls.api;
+
+public enum GameStatus {
+
+    IN_GAME,
+    IN_LOBBY,
+    SPECTATOR,
+    EDIT_MODE
+}
---
+++ api/src/main/java/me/jonakls/api/kits/Kit.java
@@ -0,0 +1,14 @@
+package me.jonakls.api.kits;
+
+import org.bukkit.inventory.ItemStack;
+
+public class Kit {
+
+    private String name;
+    private String description;
+    private String permission;
+    private double price;
+    private boolean status;
+    private ItemStack[] content;
+
+}
