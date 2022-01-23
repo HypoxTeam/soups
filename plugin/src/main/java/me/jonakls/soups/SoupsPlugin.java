5a96a78ec54631b6456dde2a0f12207eb976b648
Jonakls <asmot54@gmail.com>
2022-01-23 11:23:24 -0500

    chore: i dont remember it changing LOL

--- plugin/src/main/java/me/jonakls/soups/SoupsPlugin.java
+++ plugin/src/main/java/me/jonakls/soups/SoupsPlugin.java
@@ -4,15 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SoupsPlugin extends JavaPlugin {
 
-	
-    @Override
-    public void onDisable() {
-	// On disable logic plugin
-    }
-
     @Override
     public void onEnable() {
         // On enablo logic plugin
+    }
+
+    @Override
+    public void onDisable() {
+        // On disable logic plugin
     }
 
 }
