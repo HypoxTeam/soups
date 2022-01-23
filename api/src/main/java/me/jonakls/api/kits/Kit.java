2ed264933b7091ae910fc3cba8d857945543831e
Jonakls <asmot54@gmail.com>
2022-01-23 11:21:28 -0500

    feat: added getters and constructor of kits

--- api/src/main/java/me/jonakls/api/kits/Kit.java
+++ api/src/main/java/me/jonakls/api/kits/Kit.java
@@ -11,4 +11,60 @@ public class Kit {
     private boolean status;
     private ItemStack[] content;
 
+    public Kit(String name, String description, String permission, double price, boolean status, ItemStack[] content) {
+        this.name = name;
+        this.description = description;
+        this.permission = permission;
+        this.price = price;
+        this.status = status;
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public ItemStack[] getContent() {
+        return content;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void setContent(ItemStack[] content) {
+        this.content = content;
+    }
 }
