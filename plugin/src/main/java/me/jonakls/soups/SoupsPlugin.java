36b379ce057ce56f15a0480e8fae94606ea5a40b
Jonakls <asmot54@gmail.com>
2022-01-29 15:02:49 -0500

    feat: added binder module, it help inject instancies on load plugin

--- plugin/src/main/java/me/jonakls/soups/SoupsPlugin.java
+++ plugin/src/main/java/me/jonakls/soups/SoupsPlugin.java
@@ -1,17 +1,44 @@
 package me.jonakls.soups;
 
+import me.jonakls.api.EnchantmentData;
+import me.jonakls.api.builders.ItemBuilder;
+import me.jonakls.soups.module.BinderModule;
+import me.yushust.inject.Injector;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SoupsPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // On enablo logic plugin
+        setupInjector();
+        ItemStack si = new ItemBuilder(Material.DIAMOND_AXE)
+                .setName("Espada")
+                .setLore("Hello world", "This is a lore of item")
+                .addEnchants(
+                        new EnchantmentData(Enchantment.DAMAGE_ALL, 4),
+                        new EnchantmentData(Enchantment.DURABILITY, 3)
+                )
+                .addFlags(ItemFlag.HIDE_ATTRIBUTES)
+                .setUnbreakable(true)
+                .build();
+
+
+
     }
 
     @Override
     public void onDisable() {
         // On disable logic plugin
+    }
+
+
+    private void setupInjector() {
+        Injector injector = Injector.create(new BinderModule(this));
+        injector.injectMembers(this);
     }
 
 }
--- plugin/src/main/java/me/jonakls/soups/module/BinderModule.java
+++ plugin/src/main/java/me/jonakls/soups/module/BinderModule.java
@@ -1,2 +1,18 @@
-package me.jonakls.soups.module;public class BinderModule {
+package me.jonakls.soups.module;
+
+import me.jonakls.soups.SoupsPlugin;
+import me.yushust.inject.AbstractModule;
+
+public class BinderModule extends AbstractModule {
+
+    private final SoupsPlugin plugin;
+
+    public BinderModule(SoupsPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    protected void configure() {
+        bind(SoupsPlugin.class).toInstance(plugin);
+    }
 }
