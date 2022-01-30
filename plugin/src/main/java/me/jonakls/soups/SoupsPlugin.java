bd930ef7344db7bdd17541019440b31f2a2c2b7b
jonakls <asmot54@hotmail.com>
2022-01-30 10:22:19 -0500

    chore: remove unnecessary item building

--- plugin/src/main/java/me/jonakls/soups/SoupsPlugin.java
+++ plugin/src/main/java/me/jonakls/soups/SoupsPlugin.java
@@ -1,13 +1,7 @@
 package me.jonakls.soups;
 
-import me.jonakls.api.EnchantmentData;
-import me.jonakls.api.builders.ItemBuilder;
 import me.jonakls.soups.module.BinderModule;
 import me.yushust.inject.Injector;
-import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemFlag;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SoupsPlugin extends JavaPlugin {
@@ -15,16 +9,6 @@ public class SoupsPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         setupInjector();
-        ItemStack si = new ItemBuilder(Material.DIAMOND_AXE)
-                .setName("Espada")
-                .setLore("Hello world", "This is a lore of item")
-                .addEnchants(
-                        new EnchantmentData(Enchantment.DAMAGE_ALL, 4),
-                        new EnchantmentData(Enchantment.DURABILITY, 3)
-                )
-                .addFlags(ItemFlag.HIDE_ATTRIBUTES)
-                .setUnbreakable(true)
-                .build();
 
 
 
