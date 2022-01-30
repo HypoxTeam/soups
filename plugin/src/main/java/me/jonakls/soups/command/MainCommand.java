eef608c7ee75ce49d9ca7d199f4dcc487ed9a6dc
jonakls <asmot54@hotmail.com>
2022-01-30 10:22:54 -0500

    feat: added general command of plugin

--- plugin/src/main/java/me/jonakls/soups/command/MainCommand.java
+++ plugin/src/main/java/me/jonakls/soups/command/MainCommand.java
@@ -1,2 +1,20 @@
-package me.jonakls.soups.command;public class MainCommand {
+package me.jonakls.soups.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.List;
+
+public class MainCommand implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(CommandSender sender,Command command, String s, String[] args) {
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        return null;
+    }
 }
