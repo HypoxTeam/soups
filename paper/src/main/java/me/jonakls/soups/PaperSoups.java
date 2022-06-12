128975be70848ba40583664c976afc8b217bc023
jonakls <asmot54@hotmail.com>
2022-06-12 16:27:56 -0500

    feat: noneeee!!

--- paper/src/main/java/me/jonakls/soups/PaperSoups.java
+++ paper/src/main/java/me/jonakls/soups/PaperSoups.java
@@ -1,11 +1,13 @@
 package me.jonakls.soups;
 
+import me.jonakls.soups.listeners.PlayerJoinMessagesListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PaperSoups extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(new PlayerJoinMessagesListener(), this);
         getLogger().info("Hi");
     }
 
