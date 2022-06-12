730c8fae4010d22b88c667c6f7d4e57aee7174f5
jonakls <asmot54@hotmail.com>
2022-06-12 09:13:11 -0500

    feat: added main clas of paper plugin

---
+++ paper/src/main/java/me/jonakls/soups/PaperSoups.java
@@ -0,0 +1,12 @@
+package me.jonakls.soups;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PaperSoups extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hi");
+    }
+
+}
