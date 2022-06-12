105422643894983dc20fbd86f41507b3b9f60467
jonakls <asmot54@hotmail.com>
2022-06-12 16:23:25 -0500

    feat: added examples of classes that injected with guice

--- spigot/src/main/java/me/jonakls/soups/command/MainCommand.java
+++ spigot/src/main/java/me/jonakls/soups/command/MainCommand.java
@@ -1,5 +1,8 @@
 package me.jonakls.soups.command;
 
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import me.jonakls.soups.configuration.YamlFile;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,6 +11,13 @@ import org.bukkit.command.TabCompleter;
 import java.util.List;
 
 public class MainCommand implements CommandExecutor, TabCompleter {
+
+    @Inject @Named("config")
+    private YamlFile config;
+
+    @Inject @Named("lang")
+    private YamlFile lang;
+
     @Override
     public boolean onCommand(CommandSender sender,Command command, String s, String[] args) {
         return false;
