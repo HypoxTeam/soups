7f82aa9a74df2b81dea1a5775d927baba5e935a5
jonakls <asmot54@hotmail.com>
2022-06-12 16:30:57 -0500

    feat: added module of guice and added injection members

--- spigot/src/main/java/me/jonakls/soups/SoupsPlugin.java
+++ spigot/src/main/java/me/jonakls/soups/SoupsPlugin.java
@@ -1,28 +1,43 @@
 package me.jonakls.soups;
 
+import com.google.inject.Guice;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
+import me.jonakls.api.manager.ManagerLoader;
+import me.jonakls.soups.managers.ListenerManager;
 import me.jonakls.soups.module.BinderModule;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SoupsPlugin extends JavaPlugin {
+
+    @Inject
+    private ListenerManager listenerManager;
 
     @Override
     public void onEnable() {
         setupInjector();
-
-
-
+        setupManagers();
     }
+
+
 
     @Override
     public void onDisable() {
         // On disable logic plugin
     }
 
+    private void setupManagers() {
+        List<ManagerLoader> managers = Arrays.asList(listenerManager);
+        for (ManagerLoader managerLoader : managers) {
+            managerLoader.start();
+        }
+    }
 
     private void setupInjector() {
-        BinderModule binderModule = new BinderModule(this);
-        Injector injector = binderModule.createInjector();
+        Injector injector = Guice.createInjector(new BinderModule(this));
         injector.injectMembers(this);
     }
 
